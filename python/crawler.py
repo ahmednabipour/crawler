9e6923c6267fd5d8999193e362191a6f340b58bd
ahmednabipour <ahmednabipour@yahoo.com>
2018-06-20 20:44:22 +0430

    da

--- python/crawler.py
+++ python/crawler.py
@@ -16,7 +16,7 @@ ser = serial.Serial()
 ser.baudrate = 9600
 ser.port = 'COM14'              # find out that ardunio is connect to whcih one of the COMs (you can use arduino IDE)
 ser.open()
-while True:
+while True:                     # make sure that arduino is ready for serial communication
     income = ser.readline()
     ser.flush() 
     serial_stat = income.decode('utf-8')
@@ -53,18 +53,17 @@ def f(params):
     roll_forward_func(trace_back_func, reward)
     return reward 
 
-def greedy():
+def greedy():           # this is the greedy function which tell the robot to do the best action which u know until now
     if iteration == 1:
-        optimize.minimize(f, initial_guess,(),'Nelder-Mead',None,None,5)
-        print("iter aval tamoom shooddaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-    else:
-        print("in iteration number 2")
+        optimize.minimize(f, initial_guess,(),'Nelder-Mead',None,None,5)        # we have changed the simplex method constants as the following:
+        print("first iteration has been finished")                              # rho = 1.2  chi = 2     psi = 0.5   sigma = 0.5     nonzdelt = 1    zdelt = 0.00025
+    else:                                                                       # we have also changed some parts of the scipy nelder_mead library. The nones are those changed
+        print("I am not in first iteration any more")                           # parts which are not needed. in this line 8 is the max_iteration and trace_back_state is the initial simplex
         optimize.minimize(f, initial_guess,(),'Nelder-Mead',None,None,8,trace_back_state)
 
-def explore():
+def explore():          # makes the robot to explore so to make sure the robot has seen all the states
     while True:
-        print("###########################################################################################")
-        #np.random.seed(47)
+        print("I am exploring")
         servo1_angle = random.SystemRandom().randint(0 , 90)
         servo2_angle = random.SystemRandom().randint(0 , 110)
         if servo1_angle not in state1_reward_dict.keys() and servo2_angle not in state2_reward_dict.keys():
@@ -72,14 +71,14 @@ def explore():
             print(encoder_reward)
             reward_max = min(state1_reward_dict.values())
             if encoder_reward <= reward_max :
-                print("i did it********************************************************")
+                print("I found a better solution")
                 initial_guess = [servo1_angle, servo2_angle]
                 optimize.minimize(f, initial_guess,(),'Nelder-Mead',None,None,5)
             state1_reward_dict[servo1_angle] = encoder_reward
             state2_reward_dict[servo2_angle] = encoder_reward
             break                
 
-def arduino(servo1, servo2):
+def arduino(servo1, servo2):        #send the string which contains 2 numbers, the angle of Servo1 and 2
     servo1 = str(servo1)
     servo2 = str(servo2)
     out = servo1 + "&" + servo2
@@ -92,10 +91,9 @@ def arduino(servo1, servo2):
     return (reward)
 
 
-while 1 :
-    #np.random.seed(47)
-    servo1_angle = random.SystemRandom().randint(0 , 90)
-    servo2_angle = random.SystemRandom().randint(0 , 110)
+while 1 :               # the infinit loop of learning :D
+    servo1_angle = random.SystemRandom().randint(0 , 90)        #randomly tryes some angles inorder to find an angle which the robot moves forward
+    servo2_angle = random.SystemRandom().randint(0 , 110)       #after that it tries to learn better solutins by using its experience and downhill simplex method
     if servo1_angle not in state1_reward_dict.keys() and servo2_angle not in state2_reward_dict.keys():                     
         encoder_reward = arduino(servo1_angle, servo2_angle) #  take reward from arduino
         initial_guess = [servo1_angle, servo2_angle]
